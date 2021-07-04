aaf605ab3a4b7b85b860287f24b1459ff32cba17
Oscar Escamilla <escamillaluquenoo@gmail.com>
2021-07-04 13:39:57 -0500

    change access controll origin axios

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-const config = {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "text/plain",
-    },
-  };
+// const config = {
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//       "Content-Type": "text/plain",
+//     },
+//   };
 
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3",
-    htttps: config,
+  //  htttps: config,
 })
 
 
