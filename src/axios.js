5c7309acaf4d8d93963498234bc19459667e681f
Oscar Escamilla <escamillaluquenoo@gmail.com>
2021-07-03 19:16:42 -0500

    first commit and render movie's image

---
+++ src/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "text/plain",
+    },
+  };
+
+const instance = axios.create({
+    baseURL: "http://api.themoviedb.org/3",
+    htttps: config,
+})
+
+
+export default instance
